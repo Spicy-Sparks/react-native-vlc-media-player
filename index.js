608f4bc63e85325b287f7e885eecbc0fe5e34a9c
Alessandro Gerelli <geroale2000@gmail.com>
2021-09-25 11:22:59 +0200

    Fix

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-
-const VLCPlayerControl = {
-  VLCPlayer: require('./VLCPlayer').default,
-  VlCPlayerView: require('./playerView/index').default,
-}
-
-module.exports = VLCPlayerControl;
+module.exports = require('./VLCPlayer').default;
